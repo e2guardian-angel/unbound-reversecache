fadedf2e200370a18ebccb6d6aa8a005aff40d52
justinschw <justinmschw@gmail.com>
2023-03-01 05:54:26 -0600

    entrypoint fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,10 @@ UNBOUND_CONF_UNSAFE=${UNBOUND_CONF_DIR}/unbound-unsafe.conf
 
 if [ "${FORWARDER}" = "true" ]; then
 	cp $UNBOUND_CONF_FORWARDER.tmpl $UNBOUND_CONF_FORWARDER
-	DNS_IP=$(extract_value "${CONFIG}" dnsIP)
 	sed -i "s~DNS_REVERSE_SVC_IP~$DNS_REVERSE_SERVICE_HOST~g" $UNBOUND_CONF_FORWARDER
     export UNBOUND_CONF=${UNBOUND_CONF_FORWARDER}
 else
-    if [ "${SAFESEARCH}" = "true" ]; then
+    if [ "${SAFESEARCH}" = "yes" ]; then
         export UNBOUND_CONF=${UNBOUND_CONF_SAFE}
     else
         export UNBOUND_CONF=${UNBOUND_CONF_UNSAFE}
